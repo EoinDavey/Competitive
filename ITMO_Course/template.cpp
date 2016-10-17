b2f22857542d3e3d55c039aabfcf28138dcdb0ca
Eoin Davey <eoind@vey.ie>
2016-10-17 14:34:06 +0100

    Add ITMO Course

---
+++ ITMO_Course/template.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<cmath>
+#include<string>
+#include<vector>
+#include<fstream>
+#ifdef JUDGE
+std::ifstream cin("aplusb.in");
+std::ofstream cout("aplusb.out");
+#endif
+
+using namespace std;
+
+int main(){
+    return 0;
+}
