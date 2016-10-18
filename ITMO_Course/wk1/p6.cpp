e3f96c45a73dc956c1682b94833ebe9f81581542
Eoin Davey <eoind@vey.ie>
2016-10-18 22:19:38 +0100

    update wk1 p6

--- ITMO_Course/wk1/p6.cpp
+++ ITMO_Course/wk1/p6.cpp
@@ -19,7 +19,7 @@ using namespace std;
 
 int W,H;
 
-map<char,pair<int,int> > board;
+pair<int,int> board[200];
 
 int main(){
     cin >> W >> H;
@@ -29,31 +29,27 @@ int main(){
     for(int i = 0; i < H; i++){
         getline(cin,s);
         for(int j = 0; j < W; j++){
-            board[s[i]] = make_pair(i,j);
+            board[((int)s[j])] = make_pair(i,j);
         }
     }
-    cout << "finished" << endl;
     string names[3];
     getline(cin,names[0]);
     int mn = -1;
     int ans = 0;
     for(int i = 0; i < 3; i++){
         getline(cin,names[i]);
-        cout << "Name " << names[i] << endl;
         int cost,prevX,prevY;
         cost=0;
         prevX=prevY=-1;
         while(getline(cin,s),s!=""){
-            cout << s << endl;
             for(int j = 0; j < s.length(); j++){
-                pair<int,int> p = board[s[j]];
+                pair<int,int> p = board[((int)s[j])];
                 if(prevX!=-1){
                     cost+= max(abs(p.first-prevX),abs(p.second-prevY));
                 }
                 prevX = p.first;prevY=p.second;
             }
         }
-        cout << cost << endl;
         if(cost<mn||mn==-1)mn=cost,ans=i;
     }
     cout << names[ans] << endl << mn << endl;
