6226dc008620c07b659f847eead5ec5d659a9284
Eoin Davey <eoind@vey.ie>
2017-10-01 00:59:06 +0100

    Kattis
    Oceans Anti 11
    Oceans Anti 11 Hard

---
+++ Kattis/Oceans_Anti_11.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+constexpr int MX_M = 11;
+constexpr int MX_N = 10001;
+constexpr int MD = 1000000007;
+char inp[] = "11";
+int N,M;
+int dfa[2][MX_M];
+int memo[MX_M][MX_N];
+
+int dp(int u, int n){
+    if(memo[u][n] != -1)
+        return memo[u][n];
+    if(n==0)
+        return u!=M;
+    return memo[u][n] = ((dp(dfa[0][u],n-1) % MD) + (dp(dfa[1][u],n-1) %MD)) %MD;
+}
+
+int main(){
+    int T; scanf("%d",&T);
+    memset(dfa,0,sizeof(dfa));
+    memset(memo,-1,sizeof(memo));
+    M = strlen(inp);
+    dfa[inp[0]-'0'][0] = 1;
+    int X = 0;
+    for(int j = 1; j < M; ++j){
+        dfa[0][j] = dfa[0][X];
+        dfa[1][j] = dfa[1][X];
+        dfa[inp[j]-'0'][j] = j+1;
+        X = dfa[inp[j]-'0'][X];
+    }
+    dfa[0][M] = dfa[1][M] = M;
+    while(T-->0){
+        scanf("%d",&N);
+        printf("%d\n",dp(0,N));
+    }
+    return 0;
+}
