a6da804f9a375ebafe0e9504bfa2780a4aec0425
Eoin Davey <eoind@vey.ie>
2017-10-08 23:35:45 +0100

    Faster emptying the baltic

--- Kattis/Emptying_The_Baltic.cpp
+++ Kattis/Emptying_The_Baltic.cpp
@@ -1,19 +1,7 @@
-#include<bits/stdc++.h>
+#include<cstdio>
+#include<queue>
 using namespace std;
 typedef long long ll;
-
-constexpr int MX_HW = 505, INF=100000000;
-int H,W;
-int dx,dy;
-int board[MX_HW][MX_HW];
-int dist[MX_HW][MX_HW];
-
-int moves[][2]={{0,1},{0,-1},{1,0},{-1,0},{1,1},{1,-1},{-1,1},{-1,-1}};
-
-bool valid(int i, int j){
-    return i>=0&&j>=0&&i<H&&j<W && board[i][j] > 0;
-}
-
 struct path{
     int x,y,d;
     path(int _x, int _y, int _d) : x(_x), y(_y), d(_d){}
@@ -22,19 +10,49 @@ struct path{
     }
 };
 
+constexpr int MX_HW = 505, INF=100000000;
+int H,W;
+int dx,dy;
+int board[MX_HW][MX_HW];
+int dist[MX_HW][MX_HW];
+
+constexpr int moves[][2]={{0,1},{0,-1},{1,0},{-1,0},{1,1},{1,-1},{-1,1},{-1,-1}};
+
+inline bool valid(int i, int j){
+    return i>=0&&j>=0&&i<H&&j<W && board[i][j] > 0;
+}
+
+inline int read_int(){
+    char c; int o=0;
+    bool ng=false;
+    while(1){
+        c = getchar_unlocked();
+        if(c=='-')
+            ng=true;
+        if(c>='0' && c <= '9')
+            break;
+    }
+    do{
+        o = o*10 + c-'0';
+        c = getchar_unlocked();
+    }while(c>='0' && c <= '9');
+    return ng ? -o : o;
+}
+
 int main(){
-    scanf("%d %d",&H,&W);
+    H = read_int();
+    W = read_int();
     for(int i = 0; i < H; ++i)
         for(int j = 0; j < W; ++j)
-            scanf("%d",&board[i][j]);
+            board[i][j]=read_int();
     for(int i = 0; i < H; ++i)
         for(int j = 0; j < W; ++j)
             board[i][j]*=-1;
     for(int i = 0; i< H; ++i)
         for(int j = 0; j < W;++j)
             dist[i][j] = INF;
-    scanf("%d %d",&dx,&dy);
-    --dx,--dy;
+    dx = read_int()-1;
+    dy = read_int()-1;
     dist[dx][dy] = 0;
     priority_queue<path> q;
     q.push(path(dx,dy,0));
