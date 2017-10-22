cc68242bae3ef96c70d1b746f40719947a6d6bff
Eoin Davey <eoind@vey.ie>
2017-10-22 22:06:11 +0100

    Fix UKIEPC b

--- UKIEPC_2017/b.cpp
+++ UKIEPC_2017/b.cpp
@@ -2,29 +2,33 @@
 using namespace std;
 
 struct point {
-    int x,y;
-    point(int _x, int _y) : x(_x), y(_y){}
+    double x,y;
+    point(double _x, double _y) : x(_x), y(_y){}
     point(){}
-    point operator+ (const point& p)const {
+
+    point operator+ (const point& p) const{
         return point(x+p.x,y+p.y);
     }
+
     point operator- (const point& p) const{
         return point(x-p.x,y-p.y);
     }
+
     bool operator== (const point& p) const {
         return x==p.x && y==p.y;
     }
 };
 
+point anchor;
 constexpr int MX_N = 100001;
 int N;
 point vertices[MX_N];
 
 double dist(const point& a, const point& b){
-    return sqrt(pow(a.x-b.x,2) + pow(a.y-b.y,2));
+    return hypot(a.x-b.x,a.y-b.y);
 }
 
-int cross(const point& a, const point& b){
+double cross(const point& a, const point& b){
     return a.x*b.y - a.y*b.x;
 }
 
@@ -33,23 +37,31 @@ bool ccw(const point& a, const point& b, const point& c){
         return false;
     point u = b-c;
     point v = b-a;
-    int cr = cross(u,v);
-    return cr >= 0;
+    double cr = cross(u,v);
+    return cr >= 0.0;
 }
 
-int dot(const point& a, const point& b){
+bool angleCmp(const point& a, const point& b){
+    point relA = a-anchor;
+    point relB = b-anchor;
+    if(cross(relA,relB)==0)
+        return dist(a,anchor) > dist(b,anchor);
+    return atan2(relA.y,relA.x) < atan2(relB.y, relB.x);
+}
+
+double dot(const point& a, const point& b){
     return a.x*b.x + a.y*b.y;
 }
 
-int norm_sq(const point& a){
-    return  a.x*a.x + a.y*a.y;
+double norm_sq(const point& a){
+    return a.x*a.x + a.y*a.y;
 }
 
 double distToLine(const point& p, const point &a, const point& b){
     point ap = p-a;
     point ab = b-a;
     point c;
-    double u = ((double) dot(ap,ab))/ ((double) norm_sq(ab));
+    double u = dot(ap,ab)/ norm_sq(ab);
     c = a + point(u*ab.x, u*ab.y);
     return dist(p,c);
 }
@@ -57,7 +69,16 @@ double distToLine(const point& p, const point &a, const point& b){
 int main() {
     scanf("%d",&N);
     for(int i = 0; i < N; ++i)
-        scanf("%d %d",&vertices[i].x,&vertices[i].y);
+        scanf("%lf %lf",&vertices[i].x,&vertices[i].y);
+    int pos = 0;
+    for(int i = 0; i < N; i++)
+        if(vertices[i].y < vertices[pos].y || (vertices[i].y==vertices[pos].y && (vertices[i].x > vertices[pos].x)))
+            pos = i;
+    point _tp = vertices[0];
+    vertices[0] = vertices[pos];
+    vertices[pos] = _tp;
+    anchor = vertices[0];
+    sort(vertices+1,vertices+N, angleCmp);
     vector<point> hull;
     hull.push_back(vertices[N-1]);
     hull.push_back(vertices[0]);
@@ -70,19 +91,12 @@ int main() {
             hull.pop_back();
     }
     double mn = 1000000000.0;
-    putchar('\n');
-    for(int i = 0; i < hull.size(); ++i)
-        printf("%d %d\n",hull[i].x,hull[i].y);
-    putchar('\n');
     for(int i = 0; i < hull.size()-1; ++i){
-        printf("Edge (%d,%d), (%d,%d)\n",hull[i].x,hull[i].y,hull[i+1].x,hull[i+1].y);
         double mx = 0.0;
         for(int j = 0; j < N; ++j){
             double d = distToLine(vertices[j],hull[i],hull[i+1]);
-            printf("D to (%d,%d)\n:%lf\n",vertices[j].x,vertices[j].y,d);
             mx = max(d,mx);
         }
-        printf("mx %lf\n",mx);
         mn = min(mn,mx);
     }
     printf("%.7lf\n",mn);
