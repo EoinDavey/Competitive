5f8b493ff3dab7f72505af566e95b0b087e331ee
Eoin Davey <eoind@vey.ie>
2018-10-06 21:12:57 -0400

    Faste ebony ivory

--- Kattis/NAQ/Ebony_Ivory.cpp
+++ Kattis/NAQ/Ebony_Ivory.cpp
@@ -5,7 +5,7 @@ const int B_SZ = 90;
 const int INF = 1000*1000;
 
 //0 ww, 1 wb, 2 bw, 3 bb
-vector<int> moves[4][10][10];
+int moves[4][10][10][13];
 
 int ww,wb,bw,bb, L;
 bool black[B_SZ];
@@ -30,13 +30,11 @@ int cost(int n1, int n2, int f1, int f2){
     if(S[n1] < S[n2]){
         int mvInd = mvMap(n1,n2);
         int dist = S[n2]-S[n1];
-        if(moves[mvInd][f1][f2].size() > 1)
-            return moves[mvInd][f1][f2][dist];
+        return moves[mvInd][f1][f2][dist] == -1 ? INF : moves[mvInd][f1][f2][dist];
     } else {
         int mvInd = mvMap(n2,n1);
         int dist = S[n1]-S[n2];
-        if(moves[mvInd][f2][f1].size() > 1)
-            return moves[mvInd][f2][f1][dist];
+        return moves[mvInd][f2][f1][dist] == -1 ? INF : moves[mvInd][f2][f1][dist];
     }
     return INF;
 }
@@ -55,6 +53,7 @@ int dp(int cf, int pos){
 
 int main(){
     memset(memo,-1,sizeof(memo));
+    memset(moves,-1,sizeof(moves));
     black[2]=black[5]=black[7]=black[10]=black[12] = true;
     for(int i = 0; i < B_SZ; ++i){
         if(black[i] && i + 12 < B_SZ)
@@ -66,17 +65,14 @@ int main(){
     for(int k = 0; k < 4; ++k){
         for(int i = 0; i < sizes[k]; ++i){
             scanf("%d %d",&fl,&fu);
-            vector<int> vi(13);
             for(int j = 1; j <= 12; ++j){
-                scanf("%d",&h);
-                vi[j] = h;
+                scanf("%d",&moves[k][fl][fu][j]);
             }
-            moves[k][fl][fu] = vi;
         }
     }
+    S.resize(L);
     for(int i = 0; i < L; ++i){
-        scanf("%d",&h);
-        S.push_back(h);
+        scanf("%d",&S[i]);
     }
     int mn = INF;
     for(int i = 1; i<=5; ++i)
