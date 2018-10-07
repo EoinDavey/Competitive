935f21e144235bcb3745c43743afffa3f1ad5807
Eoin Davey <eoind@vey.ie>
2018-10-06 20:43:57 -0400

    Fix Ebony Ivory

--- Kattis/NAQ/Ebony_Ivory.cpp
+++ Kattis/NAQ/Ebony_Ivory.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 const int MX_SZ = 22;
 const int B_SZ = 90;
-const int INF = 1000*1000*1000;
+const int INF = 1000*1000;
 
 //0 ww, 1 wb, 2 bw, 3 bb
-map<int, map<int, vector<int> > > moves[4];
+vector<int> moves[4][10][10];
 
-int ww,wb,bw,bb;
+int ww,wb,bw,bb, L;
 bool black[B_SZ];
-string S;
+vector<int> S;
 
-int memo[5][10011];
+int memo[10][10011];
 
 int mvMap(int a, int b){
     if(black[S[a]]){
@@ -25,19 +25,17 @@ int mvMap(int a, int b){
 }
 
 int cost(int n1, int n2, int f1, int f2){
-    if(n1==n2)
+    if(S[n1]==S[n2])
         return f1==f2 ? 0 : INF;
     if(S[n1] < S[n2]){
         int mvInd = mvMap(n1,n2);
-        int dist = S[n1]-S[n2];
-        if(moves[mvInd].find(f1) != moves[mvInd].end()
-                && moves[mvInd][f1].find(f2) != moves[mvInd][f1].end())
+        int dist = S[n2]-S[n1];
+        if(moves[mvInd][f1][f2].size() > 1)
             return moves[mvInd][f1][f2][dist];
     } else {
         int mvInd = mvMap(n2,n1);
-        int dist = S[n2]-S[n1];
-        if(moves[mvInd].find(f2) != moves[mvInd].end()
-                && moves[mvInd][f2].find(f1) != moves[mvInd][f2].end())
+        int dist = S[n1]-S[n2];
+        if(moves[mvInd][f2][f1].size() > 1)
             return moves[mvInd][f2][f1][dist];
     }
     return INF;
@@ -63,21 +61,26 @@ int main(){
             black[i+12]=true;
     }
     int sizes[4];
-    scanf("%d %d %d %d",&sizes[0], sizes+1, sizes+2, sizes+3);
+    scanf("%d %d %d %d %d",&sizes[0], sizes+1, sizes+2, sizes+3, &L);
     int fl,fu,h;
     for(int k = 0; k < 4; ++k){
-        for(int i = 0; i < ww; ++i){
+        for(int i = 0; i < sizes[k]; ++i){
             scanf("%d %d",&fl,&fu);
             vector<int> vi(13);
-            vi[0] = fu;
             for(int j = 1; j <= 12; ++j){
                 scanf("%d",&h);
                 vi[j] = h;
             }
-            if(moves[0].find(fl) == moves[0].end())
-                moves[0][fl] = map<int, vector<int> >();
-            moves[0][fl][fu] = vi;
+            moves[k][fl][fu] = vi;
         }
     }
+    for(int i = 0; i < L; ++i){
+        scanf("%d",&h);
+        S.push_back(h);
+    }
+    int mn = INF;
+    for(int i = 1; i<=5; ++i)
+        mn = min(mn, dp(i,0));
+    printf("%d\n", mn);
     return 0;
 }
