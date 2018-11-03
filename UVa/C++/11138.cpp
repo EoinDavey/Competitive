2161d478d6a7523ebfd301c924078e44a74eb1e5
Eoin Davey <eoind@vey.ie>
2018-11-02 21:23:33 -0400

    Fix UVa 11138

--- UVa/C++/11138.cpp
+++ UVa/C++/11138.cpp
@@ -1,87 +1,78 @@
-#include<iostream>
-#include<cstdio>
-#include<cstring>
-#include<vector>
-#include<queue>
+#include<bits/stdc++.h>
+#define BTIND(x) x+1
+#define NTIND(x) x+MX_N
 
 using namespace std;
 
+const int MX_N = 505;
 int bolts, nuts;
-int adjMat[1010][1010];
-vector<int> adjList[1500];
-int s = 0;
-int t;
-vector<int> p;
-int f = 0;
-int INF = 1000000;
+int res[2*MX_N][2*MX_N];
+vector<int> adjList[2*MX_N];
+bool vis[2*MX_N];
+const int S = 0;
+const int T = 2*MX_N-1;
+const int INF = 1000*1000;
 
-void augment(int u, int mEdge){
-    if(u==s){
-        f = mEdge;
-        return;
-    } else if(p[u]!=-1){
-        augment(p[u], min(mEdge, adjMat[p[u]][u]));
-        if(adjMat[p[u]][u] == 0)
-            adjList[p[u]].push_back(u);
-        if(adjMat[u][p[u]] == 0)
-            adjList[u].push_back(p[u]);
-        adjMat[p[u]][u]-=f;
-        adjMat[u][p[u]]+=f;
+int f = 0;
+
+int ff(int u, int minE){
+    if(u==T)
+        return minE;
+    vis[u] = true;
+    for(int v : adjList[u]){
+        if(!vis[v] && res[u][v] > 0){
+            if(int f = ff(v, min(minE, res[u][v]))){
+                res[u][v] -=f;
+                res[v][u] +=f;
+                return f;
+            }
+        }
     }
+    return 0;
 }
 
 int main(){
-    int N,cnt;
+    int N;
     cin >> N;
-    for(cnt = 0; cnt < N; cnt++){
-        cin >> bolts >> nuts;
-        t = bolts+nuts+1;
-        for(int i = 0; i <= t; i++)
+    for(int cnt = 1; cnt <= N; ++cnt){
+        for(int i = 0; i < T; i++)
             adjList[i].clear();
+        for(int i = 0; i < 2*MX_N; ++i)
+           for(int j = 0; j <2*MX_N; ++j)
+               res[i][j]=0;
+
+        cin >> bolts >> nuts;
+
+        int b;
         for(int i = 0; i < bolts; i++){
             for(int j = 0; j < nuts; j++){
-                scanf("%d",&adjMat[i+1][j+bolts+1]);
-                if(adjMat[i+1][j+bolts+1]){
-                    adjList[i+1].push_back(j+bolts+1);
+                scanf("%d",&b);
+                if(b){
+                    adjList[BTIND(i)].push_back(NTIND(j));
+                    adjList[NTIND(j)].push_back(BTIND(i));
+                    res[BTIND(i)][NTIND(j)] = 1;
                 }
             }
         }
-        for(int i = 1; i <= bolts; i++){
-            adjMat[s][i] = 1;
-            adjList[s].push_back(i);
+        for(int i = 0; i < bolts; ++i){
+            res[S][BTIND(i)] = 1;
+            adjList[S].push_back(BTIND(i));
+            adjList[BTIND(i)].push_back(S);
         }
-        for(int i = bolts+1; i < t; i++){
-            adjMat[i][t] = 1;
-            adjList[i].push_back(t);
+        for(int i = 0; i < nuts; i++){
+            res[NTIND(i)][T] = 1;
+            adjList[NTIND(i)].push_back(T);
+            adjList[T].push_back(NTIND(i));
         }
-        vector<int> dist;
         int mf = 0;
-        while(1){
-            f = 0;
-            dist.assign(t+1,INF);
-            p.assign(t+1,-1);
-            dist[s] = 0;
-            queue<int> q;
-            q.push(s);
-            while(!q.empty()){
-                int u = q.front(); q.pop();
-                if(u==t)
-                    break;
-                for(int i = 0; i < adjList[u].size(); i++){
-                    int v = adjList[u][i];
-                    if(dist[v]==INF && adjMat[u][v] > 0){
-                        dist[v] = dist[u] + 1;
-                        p[v] = u;
-                        q.push(v);
-                    }
-                }
-            }
-            augment(t,INF);
+        for(;;){
+            memset(vis, 0, sizeof(vis));
+            int f = ff(S, INF);
             if(f==0)
                 break;
             mf+=f;
         }
-        printf("Case %d: a maximum of %d nuts and bolts can be fitted together\n",cnt+1,mf);
+        printf("Case %d: a maximum of %d nuts and bolts can be fitted together\n",cnt,mf);
     }
     return 0;
 }
