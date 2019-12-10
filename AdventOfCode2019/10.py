1b7b8657c103ced212a345321730f9ea05090be9
Eoin Davey <eoind@vey.ie>
2019-12-10 18:17:38 +0000

    More obfus

--- AdventOfCode2019/10.py
+++ AdventOfCode2019/10.py
@@ -1,8 +1,9 @@
-import sys, math as m, itertools as i
-u,l=[x.strip() for x in sys.stdin],len
-H,W,S,p,r=l(u),l(u[0]),'Part',i.product,range
+import sys,math as m,itertools as i
+l,F,e=len,filter,enumerate
+S='Part'
+s=[(x[0],y[0])for x in e(sys.stdin)for y in e(x[1].strip())if y[1]=='#']
 def R(a,b):g=m.gcd(a,b);return m.atan2((-1.0*b)/g,a/g)
-M,X,Y=max([(l(set([R(a-x,b-y)for(a,b)in p(r(H),r(W))if(a,b)!=(x,y)and u[a][b]=='#'])),x,y)for(x,y)in p(r(H),r(W))if u[x][y]=='#'])
+M,X,Y=max([(l(set([R(a-x,b-y)for(a,b)in s if(a,b)!=(x,y)])),x,y)for(x,y)in s])
 f=lambda x:R(x[0]-X,x[1]-Y)
-pt=list(filter(lambda x:x,i.chain.from_iterable(i.zip_longest(*[list(x[1])for x in i.groupby(sorted([(a,b)for (a,b)in p(r(H),r(W))if(a,b)!=(X,Y)and u[a][b]=='#'],key=f),key=f)]))))[199]
+pt=list(F(lambda x:x,i.chain.from_iterable(i.zip_longest(*[list(x[1])for x in i.groupby(sorted(F(lambda a:a!=(X,Y),s),key=f),key=f)]))))[199]
 print(S,'A',M,S,'B',pt[1]*100+pt[0])
---
+++ AdventOfCode2019/old10.py
@@ -0,0 +1,55 @@
+import sys
+from collections import defaultdict
+from math import gcd, atan2
+board = list(map(lambda x: x.strip(), sys.stdin))
+H = len(board)
+W = len(board[0])
+
+def a():
+    mx = 0
+    ax = 0
+    ay = 0
+    for x in range(H):
+        for y in range(W):
+            if board[x][y] != '#':
+                continue
+            s = set()
+            for a in range(H):
+                for b in range(W):
+                    if board[a][b] != '#' or a == x and b == y:
+                        continue
+                    g = gcd(a-x,b-y)
+                    s.add(((a-x)/g, (b-y)/g))
+            if len(s) > mx:
+                mx = len(s)
+                ax = x
+                ay = y
+    return mx, ax, ay
+
+def b(ax, ay):
+    pts = defaultdict(list)
+    for a in range(H):
+        for b in range(W):
+            if board[a][b] != '#' or a == ax and b == ay:
+                continue
+            g = gcd(a-ax,b-ay)
+            pts[((a-ax)/g, (b-ay)/g)].append((a, b))
+    keys = list(pts.keys())
+    keys.sort(key=lambda x: atan2(-x[1], x[0]))
+    for k in keys:
+        pts[k].sort(key=lambda x: -(abs(x[0]-ax) + abs(x[1]-ay)))
+    cnt = 0
+    ki = 0
+    while True:
+        if len(pts[keys[ki]]) == 0:
+            ki = (ki + 1) % len(keys)
+            continue
+        pt = pts[keys[ki]].pop()
+        cnt +=1
+        if cnt == 200:
+            return pt[1]*100 + pt[0]
+        ki = (ki + 1) % len(keys)
+
+amx, ax, ay = a()
+print('Part A', amx)
+print('Part B', b(ax,ay))
