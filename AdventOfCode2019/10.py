6f182c10978a0ab8f3a71eaccceba5094555d602
Eoin Davey <eoind@vey.ie>
2019-12-10 13:34:54 +0000

    obfus AOC 10

--- AdventOfCode2019/10.py
+++ AdventOfCode2019/10.py
@@ -1,16 +1,8 @@
-import sys
-from math import gcd, atan2
-from itertools import zip_longest, chain, islice, groupby
-board = list(map(lambda x: x.strip(), sys.stdin))
-H = len(board)
-W = len(board[0])
-
-def a():
-    return max([(len(set([(lambda g: ((a-x)//g, (b-y)//g))(gcd(a-x,b-y)) for a in range(len(board)) for b in range(W) if (a,b) != (x,y) and board[a][b] == '#'])), x, y) for x in range(len(board)) for y in range(W) if board[x][y] == '#'])
-
-def b(ax, ay):
-    return (lambda pt: pt[1]*100 + pt[0])((lambda sf: next(islice(filter(lambda x: x, chain.from_iterable(zip_longest(*map(lambda x: list(x[1]), groupby(sorted([(a,b) for a in range(H) for b in range(W) if (a!=ax or b != ay) and board[a][b] == '#'], key=sf), key=sf))))), 199, None)))(lambda x: (lambda g: (lambda h: atan2(-h[1], h[0]))(((x[0]-ax)/g, (x[1]-ay)/g)))(gcd(x[0]-ax, x[1]-ay))))
-
-amx, ax, ay = a()
-print('Part A', amx)
-print('Part B', b(ax,ay))
+import sys, math as m, itertools as i
+u,l=[x.strip() for x in sys.stdin],len
+H,W,S,p,r=l(u),l(u[0]),'Part',i.product,range
+def R(a,b):g=m.gcd(a,b);return m.atan2((-1.0*b)/g,a/g)
+M,X,Y=max([(l(set([R(a-x,b-y)for(a,b)in p(r(H),r(W))if(a,b)!=(x,y)and u[a][b]=='#'])),x,y)for(x,y)in p(r(H),r(W))if u[x][y]=='#'])
+f=lambda x:R(x[0]-X,x[1]-Y)
+pt=list(filter(lambda x:x,i.chain.from_iterable(i.zip_longest(*[list(x[1])for x in i.groupby(sorted([(a,b)for (a,b)in p(r(H),r(W))if(a,b)!=(X,Y)and u[a][b]=='#'],key=f),key=f)]))))[199]
+print(S,'A',M,S,'B',pt[1]*100+pt[0])
