7b187b9e1997a1d8f30066f47b5201f67e6de1e6
Eoin Davey <eoind@vey.ie>
2019-12-12 09:17:13 +0000

    AOC 12

--- AdventOfCode2019/10.py
+++ AdventOfCode2019/10.py
@@ -5,5 +5,5 @@ s=[(x[0],y[0])for x in e(sys.stdin)for y in e(x[1].strip())if y[1]=='#']
 def R(a,b):g=m.gcd(a,b);return m.atan2((-1.0*b)/g,a/g)
 M,X,Y=max([(l(set([R(a-x,b-y)for(a,b)in s if(a,b)!=(x,y)])),x,y)for(x,y)in s])
 f=lambda x:R(x[0]-X,x[1]-Y)
-pt=list(F(lambda x:x,i.chain.from_iterable(i.zip_longest(*[list(x[1])for x in i.groupby(sorted(F(lambda a:a!=(X,Y),s),key=f),key=f)]))))[199]
-print(S,'A',M,S,'B',pt[1]*100+pt[0])
+T=list(F(lambda x:x,i.chain.from_iterable(i.zip_longest(*[list(x[1])for x in i.groupby(sorted(F(lambda a:a!=(X,Y),s),key=f),key=f)]))))[199]
+print(S,'A',M,S,'B',T[1]*100+T[0])
---
+++ AdventOfCode2019/12.py
@@ -0,0 +1,45 @@
+import sys
+from math import gcd
+from functools import reduce
+mnps = [tuple([int(y.split('=')[1]) for y in x.strip()[1:-1].split(',')]) for x in sys.stdin]
+M = len(mnps)
+def ad(x, y):
+    return tuple([x+y for (x,y) in zip(x,y)])
+
+def a(mnps):
+    mnvs = [(0,0,0) for _ in range(M)]
+    for _ in range(1000):
+        for a in range(M):
+            for b in range(a):
+                dff = tuple((x>y)-(x<y) for (x,y) in zip(mnps[a],mnps[b]))
+                mnvs[a] = ad(mnvs[a],[-x for x in dff])
+                mnvs[b] = ad(mnvs[b],dff)
+        for k in range(M):
+            mnps[k] = ad(mnps[k], mnvs[k])
+    return sum([sum(map(abs,x))*sum(map(abs,y)) for (x,y) in zip(mnvs,mnps)])
+
+def b(mnps):
+    tms = [0]*3
+    for i in range(3):
+        ps = [x[i] for x in mnps]
+        s = set()
+        s.add((tuple(ps),(0,0,0,0)))
+        vs = [0]*M
+        t = 0
+        while True:
+            t += 1
+            for a in range(M):
+                for b in range(a):
+                    df = (ps[a] > ps[b]) - (ps[a] < ps[b])
+                    vs[a] -= df
+                    vs[b] += df
+            ps = ad(ps, vs)
+            key = (ps, tuple(vs))
+            if key in s:
+                tms[i] = t
+                break
+            s.add(key)
+    return reduce(lambda a,b:(a*b)//gcd(a,b),tms)
+
+print('Part A:', a(mnps[:]))
+print('Part B:', b(mnps))
