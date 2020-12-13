4bdd1854a953e0058db9a223ab81bc4eabf6d171
Eoin Davey <eoind@vey.ie>
2020-12-13 11:50:05 +0000

    Day 13 AOC

---
+++ AdventOfCode2020/d13.py
@@ -0,0 +1,27 @@
+import sys
+from functools import reduce
+
+def lines():
+  return [line.strip() for line in sys.stdin]
+
+ts = int(input())
+buses = input().split(',')
+
+def partA():
+  valid = [int(x) for x in buses if x != "x"]
+  mn = min(valid, key = lambda x: x - (ts % x))
+  print(mn * (mn - (ts % mn)))
+
+def partB():
+  inp = [(int(buses[i]), -i % int(buses[i])) for i in range(len(buses)) if buses[i] != "x"]
+  N = reduce(lambda x, y: x * y[0], inp, 1)
+  Ni = [N // a for a, b in inp]
+  z = [pow(Ni[i], inp[i][0] - 2, N) for i in range(len(inp))]
+  x = sum([(inp[i][1] * Ni[i] * z[i]) % N for i in range(len(inp))])
+  while x - N > ts:
+    x -= N
+  assert(all([x % a == b for a, b in inp]))
+  print(x)
+
+partA()
+partB()
