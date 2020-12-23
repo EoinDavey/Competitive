4841408c22c9b552e52b9bc16468a404cfc4d446
Eoin Davey <eoind@vey.ie>
2020-12-23 14:48:22 +0000

    Small cleanup

--- AdventOfCode2020/d23.py
+++ AdventOfCode2020/d23.py
@@ -12,37 +12,33 @@ def getDest(cur, mx, pck):
     if t not in pck:
       return t
 
-def solve(ls, nxts, moves):
+def solve(ls, moves):
   cur = ls[0]
   mx = max(ls)
+  nxts = [0]*(len(ls) + 1)
+  for i in range(len(ls)):
+    nxts[ls[i]] = ls[(i+1) % len(ls)]
   for _ in range(moves):
-    a = nxts[cur]
-    b = nxts[a]
-    c = nxts[b]
-    pck = a, b, c
+    a, b, c = nxts[cur], nxts[nxts[cur]], nxts[nxts[nxts[cur]]]
+    pck = [a, b, c]
 
     nxts[cur] = nxts[c]
 
     dst = getDest(cur, mx, pck)
     nxts[dst], nxts[c] = a, nxts[dst]
     cur = nxts[cur]
+  return nxts
 
 def partA():
-  nxts = [0]*(len(inp) + 1)
   ls = [int(x) for x in inp]
-  for i in range(len(ls)):
-    nxts[ls[i]] = ls[(i+1) % len(ls)]
-  solve(ls, nxts, 100)
+  nxts = solve(ls, 100)
   st = reduce(lambda x, _: x + str(nxts[int(x[-1])]), range(len(ls) - 1), "1")
   print(st[1:])
 
 def partB():
-  nxts = [0]*(10**6 + 1)
   ls = [int(x) for x in inp]
   ls += list(range(max(ls) + 1, 10**6 + 1))
-  for i in range(len(ls)):
-    nxts[ls[i]] = ls[(i+1) % len(ls)]
-  solve(ls, nxts, 10 ** 7)
+  nxts = solve(ls, 10 ** 7)
   print(nxts[1] * nxts[nxts[1]])
 
 partA()
