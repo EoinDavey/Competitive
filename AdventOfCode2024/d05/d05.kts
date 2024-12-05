7b0063e7b9b87f234e5d148acfa345f19f7635cb
Eoin Davey <eoind@vey.ie>
2024-12-05 16:30:54 +0000

    Rework AoC d05

--- AdventOfCode2024/d05/d05.kts
+++ AdventOfCode2024/d05/d05.kts
@@ -1,36 +1,30 @@
+fun <T> List<T>.isSortedWith(comp: Comparator<T>): Boolean {
+  return this.asSequence().zipWithNext().all{(a, b) -> comp.compare(a, b) <= 0}
+}
+
 fun solve(adjList: Map<Int, Set<Int>>, updates: List<List<Int>>, wantSorted: Boolean): Int {
-  class Data(val x: Int) : Comparable<Data> {
-    override operator fun compareTo(other: Data): Int {
+  val comp = object : Comparator<Int> {
+    override fun compare(a: Int, b: Int): Int {
       return when {
-        this.x == other.x -> 0
-        (adjList[this.x]?.contains(other.x) ?: false) -> -1
+        a == b -> 0
+        (adjList[a]?.contains(b) ?: false) -> -1
         else -> 1
       }
     }
   }
-  return updates.map{update ->
-    val sorted = update.sortedBy{x -> Data(x)}
-    if((update == sorted) == wantSorted) {
-      sorted[update.size / 2]!!
-    } else {
-      0
-    }
-  }.sum()
+  return updates
+    .filter{it.isSortedWith(comp) == wantSorted}
+    .map{update -> update.sortedWith(comp)[update.size / 2]!!}
+    .sum()
 }
-
-fun partA(adjList: Map<Int, Set<Int>>, updates: List<List<Int>>): Int = solve(adjList, updates, true)
-fun partB(adjList: Map<Int, Set<Int>>, updates: List<List<Int>>): Int = solve(adjList, updates, false)
 
 fun main(){
   val lines = generateSequence(::readlnOrNull).toList()
   val pairs: List<Pair<Int, Int>>  = lines.takeWhile{!it.isBlank()}.map{it.split("|").map{it.toInt()}}.map{Pair(it[0], it[1])}
   val updates: List<List<Int>>  = lines.takeLastWhile{!it.isBlank()}.map{it.split(",").map{it.toInt()}}
 
-  val adjList = mutableMapOf<Int, MutableSet<Int>>()
-  for((a, b) in pairs) {
-    adjList.getOrPut(a, ::mutableSetOf).add(b)
-  }
-  println(partA(adjList, updates))
-  println(partB(adjList, updates))
+  val adjList: Map<Int, Set<Int>> = pairs.groupBy{(a, _) -> a}.mapValues{e -> e.value.map{(_, b) -> b}.toSet()}
+  println("Part A: ${solve(adjList, updates, true)}")
+  println("Part A: ${solve(adjList, updates, false)}")
 }
 main()
