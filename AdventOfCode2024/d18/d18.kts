57ecffe5f60d17b7ac1aa1064fbbbb201cb48376
Eoin Davey <eoind@vey.ie>
2024-12-18 11:57:56 +0000

    AoC faster d18pB

--- AdventOfCode2024/d18/d18.kts
+++ AdventOfCode2024/d18/d18.kts
@@ -7,7 +7,8 @@ val MVS = arrayOf(Vec(0, 1), Vec(0, -1), Vec(1, 0), Vec(-1, 0))
 
 fun nbrs(u: Vec) = MVS.map{u + it}.filter{it.x in 0..<SIZE && it.y in 0..<SIZE}
 
-fun solve(ps: List<Vec>, want: Int): Int? {
+fun partA(ps: List<Vec>): Int? {
+    val want = 1024
     val blocked = ps.take(want).toSet()
     val start = Vec(0, 0)
     val end = Vec(SIZE - 1, SIZE - 1)
@@ -25,21 +26,43 @@ fun solve(ps: List<Vec>, want: Int): Int? {
     return dist[end]
 }
 
-fun partA(ps: List<Vec>) = solve(ps, 1024)
-
-fun partB(ps: List<Vec>): String {
-    var l = 1
-    var r = ps.size
-    while(l < r) {
-        val md = (l + r) / 2
-        if(solve(ps, md) == null) {
-            r = md
-        } else {
-            l = md + 1
+fun partB(ps: List<Vec>): String? {
+    val ufdsGrid = Array(SIZE){arrayOfNulls<Vec>(SIZE)}
+    fun get(u: Vec): Vec {
+        ufdsGrid[u.x][u.y]?.let{
+            val p = get(it)
+            ufdsGrid[u.x][u.y] = p
+            return p
+        }
+        return u
+    }
+    fun union(a: Vec, b: Vec) {
+        val pa = get(a)
+        val pb = get(b)
+        if(pa != pb)
+            ufdsGrid[pa.x][pa.y] = pb
+    }
+    val blockGrid = Array(SIZE){Array(SIZE){ps.size}}
+    for((i, p) in ps.withIndex()){
+        blockGrid[p.x][p.y] = i
+    }
+    for(x in 0..<SIZE) {
+        for(y in 0..<SIZE) {
+            if(blockGrid[x][y] != ps.size)
+                continue
+            val u = Vec(x, y)
+            for(v in nbrs(u).filter{blockGrid[it.x][it.y] == ps.size})
+                union(u, v)
         }
     }
-    val ans = ps[l - 1]
-    return "${ans.x},${ans.y}"
+    for(i in ps.size-1 downTo 0) {
+        val p = ps[i]
+        for(v in nbrs(p).filter{blockGrid[it.x][it.y] > i})
+            union(p, v)
+        if(get(Vec(0, 0)) == get(Vec(SIZE - 1, SIZE - 1)))
+            return "${p.x},${p.y}"
+    }
+    return null
 }
 
 fun main() {
